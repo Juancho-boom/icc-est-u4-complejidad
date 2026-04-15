3ee922202090f2b6b0950897a0cabc87b28ee24e
Juan Coronel <juanco1203m@gmail.com>
2026-04-15 15:56:05 -0500

    Add estudiantes list

--- src/App.java
+++ src/App.java
@@ -1,19 +1,33 @@
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hola, Mundo!");
         
         int[] array = new int[]{1,2,3,4,5};
         System.out.println(array[0]);
-        System.out.println(array);
+        System.out.println(array[2]);
 
-        //FOR NORMAL
-        for(int i=0;  i<array.length; i++){
-            System.out.println(array[i]);
-        }
-           //FOR EACH
-        for(int i : array){
-            System.out.println(i);
+        int cantidadEstudiantes = 1000000;
+        int cantidad = 1_000_000;
+
+        List<Estudiante> estudiantes = Generador.generarListado(cantidadEstudiantes);
+        
+        for(Estudiante est : estudiantes){
+            System.out.println(est.nombre);
         }
 
+        String nombreBuscar = "Estudiante_29000111";
+        int intentos = 0;
+
+        for(Estudiante est : estudiantes){
+            if(est.nombre.equals(nombreBuscar)){
+                System.out.println("Existe");
+            } else {
+                intentos++;
+            }
+        }
+
+        System.out.println(x: "Fin");
     }
 }
